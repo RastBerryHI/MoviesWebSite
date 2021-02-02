56fcd9b1e9b60be26d3966062f6361b1c76cfd6c
RastBerryHI <musaev.pasha@gmail.com>
2021-02-02 14:14:23 +0200

    Movies WebSite
    Accessing to all methods via address line
    TODO: Interactive buttons

---
+++ UdemyWebSite/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UdemyWebSite.Models;
+using UdemyWebSite.ViewModels;
+
+namespace UdemyWebSite.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Shrek" };
+            List<Customer> customers = new List<Customer>(){ 
+                
+                new Customer() { Name = "Customer 1"}, 
+                new Customer() { Name = "Customer 2"},
+                new Customer() { Name = "Customer 3"},
+                new Customer() { Name = "Customer 4"},
+                new Customer() { Name = "Customer 5"},
+                new Customer() { Name = "Customer 6"}
+
+            };
+
+            RandomMovieViewModel viewModel = new RandomMovieViewModel(){
+
+                Movie = movie,
+                Customers = customers
+
+            };
+
+            return View(viewModel);
+        }
+
+        /* 
+         * Attribue for Custom Route; month can contain only 2 digits 
+         * and be in range between 1 and 12. Year - only 4 digits 
+         */
+        
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")] 
+        public ActionResult ByReleaseYear(int year, byte month) 
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
